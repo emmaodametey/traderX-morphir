bbb362357ddb4e503a327c39bf421ddcbaee8859
Naa Kai <naa.kai.odametey@gmail.com>
2023-10-25 14:45:37 +0000

    refactor validation methods, remove comments

--- src/TraderX/TradeService/TradeOrderController.elm
+++ src/TraderX/TradeService/TradeOrderController.elm
@@ -1,10 +1,5 @@
 module TraderX.TradeService.TradeOrderController exposing (..)
 
--- import TraderX.TradeService.Shared.Security exposing (..)
--- import TraderX.TradeService.Shared.AccountId exposing(..)
--- import TraderX.TradeService.Shared.TradeSide exposing(..)
--- import Http
-
 type alias TradeOrder = 
     { 
         id: String,
@@ -13,51 +8,33 @@ type alias TradeOrder =
         accountId: Int,
         side: String
     }
--- type Msg
---     = Response (Result Http.Error ())
+
 type alias Url = 
     String
 
--- type HttpResponse 
---     = HttpClientErrorException: {
---         code: Int,
---         msg: String
---     }
---     | OK
-
-validateTicker : String -> (Bool, String)
+validateTicker : String -> Result String Bool
 validateTicker ticker =
     if String.isEmpty ticker then
-        (False, "Invalid")
+        Err "Invalid"
     else
-        (True, "valid")
-    -- if ticker == OK then
-    --     (True, "Validate Ticker")
-    -- else
-    --     (False, "Invalid")
-    -- case ticker of
-    --     OK -> (True, "Validate Ticker")
-    --     HttpClientErrorException -> (False, "Invalid")
-    --         let
-    --             result = 
-    --                 if response.code == 404 then
-    --                     (False, "Ticker Not Found in Reference Data")
-    --                 else
-    --                     (False, response.msg)
-validateAccount : Maybe Int -> (Bool, String)
-validateAccount account =
-    if account == Nothing then
-        (False, "Invalid")
-    else
-        (True, "valid")
+        Ok True
 
-createTradeOrder : TradeOrder -> (Bool, String)
-createTradeOrder tradeOrder = 
-    if not (fst (validateTicker tradeOrder.security)) then
-        (False, "ResourceNotFound")
-    else if not (fst (validateAccount tradeOrder.accountId)) then
-        (False, "ResourceNotFound")
-    else
-        (True, "Resource Found")
+validateAccount : Int -> Result String Bool
+validateAccount accountId =
+    case accountId of
+        0 -> Ok True
+        _ -> Err "Resource not found not : in Account service"
 
+trying : String -> String -> String
+trying str1 str2 = 
+    str1 ++ str2
+      
 
+createTradeOrder : TradeOrder -> Result String Bool
+createTradeOrder tradeOrder =
+    let 
+        validTicker = validateTicker tradeOrder.security
+    in
+    case validTicker of
+        Err _ -> validTicker
+        Ok _ -> validateAccount tradeOrder.accountId
